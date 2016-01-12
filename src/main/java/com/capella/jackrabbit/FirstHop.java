4f071c020542887469b33939b371c082d8e7ffac
Ramesh Rajendran <ramesh.rajendran@mastek.com>
2016-01-12 21:49:08 +0000

    first commit

---
+++ src/main/java/com/capella/jackrabbit/FirstHop.java
@@ -0,0 +1,29 @@
+package com.capella.jackrabbit;
+
+import org.apache.jackrabbit.core.TransientRepository;
+
+import javax.jcr.Repository;
+import javax.jcr.Session;
+
+public class FirstHop {
+
+    /**
+     * The main entry point of the example application.
+     *
+     * @param args command line arguments (ignored)
+     * @throws Exception if an error occurs
+     */
+    public static void main(String[] args) throws Exception {
+        Repository repository = new TransientRepository();
+        Session session = repository.login();
+        try {
+            String user = session.getUserID();
+
+            String name = repository.getDescriptor(Repository.REP_NAME_DESC);
+            System.out.println(
+                    "Logged in as " + user + " to a " + name + " repository.");
+        } finally {
+            session.logout();
+        }
+    }
+} 
