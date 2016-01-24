cf28169516c6192d4c5f5695becb9a40bc42e1b5
Ramesh Rajendran <ramesh.rajendran@mastek.com>
2016-01-24 04:44:12 +0000

    cleanup

--- src/test/java/com/capella/jackrabbit/JackRabbitOperationsTest.java
+++ src/test/java/com/capella/jackrabbit/JackRabbitOperationsTest.java
@@ -14,6 +14,7 @@ import java.io.*;
 import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -48,6 +49,8 @@ public class JackRabbitOperationsTest {
         jackRabbitOperations.readBinaryFile(path);
 
         File f = new File("target/Alfresco_E0_Training.pdf");
+
+        assertThat(f, is(notNullValue()));
         assertThat(f.length(), is(3003160L));
     }
 
